eb80df4ba1a5f7fe7471dd7e6edd979d16fe272c
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2024-08-05 23:47:54 +0530

    Loop with closure using Var

--- script.js
+++ script.js
@@ -26,3 +26,25 @@ const fa = findA();
 console.time("10");
 fa(6);
 console.timeEnd("10");
+
+// setTimeoutOutput
+
+function a() {
+    setTimeout(() => {
+        for (let i = 0; i < 3; i++) {
+            console.log(i);
+        }
+    }, 1000);
+}
+a();
+
+for (var i = 0; i < 4; i++) {
+    function inner(i) {
+        setTimeout(() => {
+            // for (let i = 0; i < 3; i++) {
+            console.log(i);
+            // }
+        }, 1000);
+    }
+    inner(i);
+}
