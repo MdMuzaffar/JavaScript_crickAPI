0d047e08f19b6449ccbeda84a4084c9ffcdfe706
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2024-08-31 22:37:50 +0530

    Js Practice, Truncate string

--- script.js
+++ script.js
@@ -442,3 +442,29 @@ function findrangOfNumber(startNumber, endNumber) {
 }
 
 console.log(findrangOfNumber(1, 5))
+
+
+// Reverse String
+
+function reverseString(str) {
+    if (str === "") {
+        return "";
+    } else {
+        return reverseString(str.substr(1)) + str.charAt(0);
+    }
+}
+
+console.log(reverseString("HELLO"));
+
+
+// Truncate
+
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        return str.slice(0, maxlength) + '...';
+    } else {
+        return str;
+    }
+}
+
+console.log(truncate("Muzaffar Ahmed is a Coder", 8))
